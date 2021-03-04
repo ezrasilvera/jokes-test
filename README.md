618e68c7a5443e67b09dde23c12f680fb08cf7de
Ezra Silvera <ezra@il.ibm.com>
2021-03-05 00:21:29 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Jokester
+
+Click a button to get a random hilarious dad joke! [Check it out](http://jokester.surge.sh/). 
+
+## What Supports This Hilarious App?
+
+Jokester is a simple single-page app built with HTML, CSS and JavaScript. It fetches jokes from the ["I Can Haz Dad Joke" API](https://icanhazdadjoke.com/api).
---
+++ index.js
@@ -0,0 +1,24 @@
+$( document ).ready(function() {
+    getJokeListener();
+});
+
+function getJokeListener() {
+	$("#get-joke").on("click", getJoke)
+}
+
+const getJoke = () => {
+	let headers = new Headers({'Accept': 'application/json'})
+	let request = new Request('https://icanhazdadjoke.com/', {headers: headers});
+	fetch(request)
+	  .then(function(response) {
+	    return response.json();
+	  })
+	  .then(function(jokeJson) {
+	    putJokeOnPage(jokeJson)
+	  });
+}
+
+const putJokeOnPage = (jokeJson) => {
+	$("#joke p").html(jokeJson.joke)
+}
+
