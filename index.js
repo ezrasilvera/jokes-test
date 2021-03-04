9507d809797f92f017fea4ff86af6aa3d2393364
Ezra Silvera <ezra@il.ibm.com>
2021-03-05 01:01:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
-$( document ).ready(function() {
-    getJokeListener();
-});
-
-function getJokeListener() {
-	$("#get-joke").on("click", getJoke)
-}
-
-const getJoke = () => {
-	let headers = new Headers({'Accept': 'application/json'})
-	let request = new Request('https://api.chucknorris.io/jokes/random', {headers: headers});
-	fetch(request)
-	  .then(function(response) {
-	    return response.json();
-	  })
-	  .then(function(jokeJson) {
-	    putJokeOnPage(jokeJson)
-	  });
-}
-
 const putJokeOnPage = (jokeJson) => {
 	$("#joke p").html(jokeJson.joke)
 }
 
+
+function randomFact() {
+      // We call the Web Service via AJAX
+      var xmlhttp = new XMLHttpRequest();
+      var url = "https://api.chucknorris.io/jokes/random";
+      xmlhttp.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+          var json = JSON.parse(this.responseText);
+          // We parse the JSON response
+          parseJson(json);
+        }
+      };
+
+      xmlhttp.open("GET", url, true);
+      xmlhttp.send();
+ }
+
+ function parseJson(json) {
+      var fact = "<b>" + json["value"] + "</b>";
+      document.getElementById("data").innerHTML = fact;
+ }
+
+    // Finally we add a click event listener on the logo of Chuck Norris
+    // to load a new random fact when the user will click on it
+ document.getElementById("logo").addEventListener("click", function() {
+      randomFact();
+ });
+
+ randomFact();
